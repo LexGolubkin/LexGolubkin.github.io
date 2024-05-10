68f3e5a31965cd51195b8714fd66d3de7720a2ac
lex_golubkin <aalexandr.golubkin@gmail.com>
2024-05-10 10:14:04 +0400

    expand pages added

--- js/order.js
+++ js/order.js
@@ -8,8 +8,6 @@ let doing = document.getElementById("doing");
 let order = document.getElementById("order");
 let close = document.getElementById("close");
 
-tg.expand();
-
 doing.addEventListener("click", () => {
     document.getElementById("main").style.display = "none";
     document.getElementById("form").style.display = "block";
